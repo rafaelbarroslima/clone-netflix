b2be90fa7f57df8f2706eae92b95a8df11b2911a
rafael <rafaelbarroslimaa@gmail.com>
2021-03-06 12:17:38 -0300

    add jquery para carousel, imgs dos filmes, script para fixacao do nav-bar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
-(function () {
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:false,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
+
+$(function () {
   var menu = document.getElementById('menu'); // colocar em cache
   window.addEventListener('scroll', function () {
       if (window.scrollY > 0) menu.classList.add('menuFixo'); // > 0 ou outro valor desejado
